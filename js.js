0fa4d55ee4641c3772299ac39a025c6919c8cf81
Shahar Zarfati <Shahar0151@gmail.com>
2022-11-16 19:20:57 +0200

    Added round function
    added a round function to determine ho wins - PC vs. Player

--- js.js
+++ js.js
@@ -2,4 +2,27 @@ const choices=["Rock", "Paper", "Scissors"];
 function getComputerChoice(){
     return randomItem = choices[Math.floor(Math.random()*choices.length)];
 }
-console.log(getComputerChoice());
+
+function playRound (playerSelection, computerSelection){
+    if (playerSelection==computerSelection)
+        return "Draw";
+
+    else if(playerSelection=="Rock" && computerSelection=="Paper")
+        return "You Lose! Paper beats Rock"
+    
+    else if(playerSelection=="Scissors" && computerSelection=="Rock")
+        return "You Lose! Rock beats Scissors"
+
+    else if(playerSelection=="Paper" && computerSelection=="Scissors")
+        return "You Lose! Scissors beats Paper"
+
+    else if(playerSelection=="Paper" && computerSelection=="Rock")
+        return "You Win! Paper beats Rock"
+    
+    else if(playerSelection=="Rock" && computerSelection=="Scissors")
+        return "You Win! Rock beats Scissors"
+
+    else
+        return "You Win! Scissors beats Paper"
+}
+
